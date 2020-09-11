150de8933125228f13ba933766221e55cb0d6871
Nathan Pope <dapope1979@gmail.com>
2020-09-11 08:18:47 -0600

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+Import-Module oh-my-posh
+Import-Module posh-git
+Set-Theme PowerPope
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
